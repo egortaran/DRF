077b8e57caf2a483c1d07c7f7d42540705bb038f
Egor Taran <64767049+egortaran@users.noreply.github.com>
2022-02-03 22:09:56 +0600

    lesson-1

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
 
-    'authors'
+    'authors',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authapp.UserDRF'
