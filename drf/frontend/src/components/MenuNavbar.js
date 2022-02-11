a4cb690be9512c6e6146932bc0637d7d14db4138
Egor Taran <64767049+egortaran@users.noreply.github.com>
2022-02-11 17:47:00 +0600

    Update MenuNavbar.js

--- drf/frontend/src/components/MenuNavbar.js
+++ drf/frontend/src/components/MenuNavbar.js
@@ -5,17 +5,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MenuNavbar = () => {
-return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-);
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 export default MenuNavbar;
